61cd454fb943ef963e1e139bd7baa71c8d53a72a
180107092 <55088565+180107092@users.noreply.github.com>
2019-12-23 23:14:25 +0600

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,14 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "registor");
+$sql = "DELETE FROM regis WHERE name='erkin'";
+$conn->close();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
